dcafa06f5ded9081d7739b19c3e6eb2c54960d61
gc1e12 <gc1e12@soton.ac.uk>
2014-12-04 16:36:19 +0000

    Prevent page from breaking.
    - controllers/page.php
    
    — Prevent displaying page that do not exist.
    — if page is invalid, reroute to home page.

--- controllers/page.php
+++ controllers/page.php
@@ -5,9 +5,16 @@ class Page extends Controller {
 	function display($f3) {
 		$pagename = urldecode($f3->get('PARAMS.3'));
 		$page = $this->Model->Pages->fetch($pagename);
+
+		if($page === false){
+			StatusMessage::add('Invalid Page','danger');
+			return $f3->reroute('/');
+		}
+
 		$pagetitle = ucfirst(str_replace("_"," ",str_replace(".html","",$pagename)));
 		$f3->set('pagetitle',$pagetitle);
 		$f3->set('page',$page);
+		
 	}
 
 }
