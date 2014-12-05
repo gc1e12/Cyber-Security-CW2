1ec5e664f86b9c9795176ba6cfa125d143a34760
gc1e12 <gc1e12@soton.ac.uk>
2014-12-05 20:09:32 +0000

    Fixed image upload.
    - allow only png, jpg,jpeg and bmp image file.
    - add a random unique number behind the image name, to prevent 2 image
    having the same name (collision)

--- utility/file.php
+++ utility/file.php
@@ -6,9 +6,11 @@ class File {
 		$f3 = Base::instance();
 		extract($array);
 		$directory = getcwd() . '/uploads';
+		$name = uniqid().$name; // input and unique id to prevent overwritting of file.
 		$destination = $directory . '/' . $name;
 		$webdest = '/uploads/' . $name;
 		if (move_uploaded_file($tmp_name,$destination)) {
+			chmod($destination,0644); // give read access to everyone, and read and write access for the owner of the site.
 			return $webdest;
 		} else {
 			return false;
