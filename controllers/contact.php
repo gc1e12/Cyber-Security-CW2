400b1b68777b06d5683b05472fd1a88f6c9eacdf
gc1e12 <gc1e12@soton.ac.uk>
2014-12-08 22:23:23 +0000

    Add captcha for contact us and remove hidden field
    — Hidden field contain the receiver mail address. —> this input is copy
    to the controllers/contact.php.

--- controllers/contact.php
+++ controllers/contact.php
@@ -5,12 +5,33 @@ class Contact extends Controller {
 	public function index($f3) {
 		if($this->request->is('post')) {
 			extract($this->request->data);
-			$from = "From: $from";
 
-			mail($to,$subject,$message,$from);
+			//verify captcha code
+			if ($captcha === $_SESSION['captcha_code']){
 
-			StatusMessage::add('Thank you for contacting us');
-			return $f3->reroute('/');
+				//check if subject and message is not empty
+				if ($from !== '' && $subject !== '' && $message !== ''){
+					
+					$from = "From: $from";
+					$to = "root@localhost";
+					mail($to,$subject,$message,$from);
+
+					StatusMessage::add('Thank you for contacting us');
+
+					unset($_SESSION['captcha_code']); // clear the session variable.
+
+					return $f3->reroute('/');
+				}else{
+					StatusMessage::add('Please fill in all the fields.','danger');
+				}
+				
+
+
+			}else{ // if incorrect captcha provided.
+   				StatusMessage::add('Please enter a valid captcha code','danger');
+			}
+
+				
 		}	
 	}
 
